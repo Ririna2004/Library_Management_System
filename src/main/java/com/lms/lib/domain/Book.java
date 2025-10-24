c0c30f75703ba238ef4442c2f53e11bff87ff460
Ririna2004 <antonrijoe@gmail.com>
2025-10-25 03:57:22 +0530

    edit commit

--- src/main/java/com/lms/lib/domain/Book.java
+++ src/main/java/com/lms/lib/domain/Book.java
@@ -5,17 +5,66 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "book")
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
     private String author;
+
     private String category;
+
     private int availableCopies;
 
     public Book() {}
+
     public Book(Long id, String title, String author, String category, int availableCopies) {
-        this.id = id; this.title = title; this.author = author; this.category = category; this.availableCopies = availableCopies;
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.availableCopies = availableCopies;
     }
-    // Getters and setters ...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
 }
--- src/main/java/com/lms/lib/domain/IssueRecord.java
+++ src/main/java/com/lms/lib/domain/IssueRecord.java
@@ -6,15 +6,68 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "issue_record")
 public class IssueRecord {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long bookId;
+
     private String username;
+
     private LocalDate issueDate;
+
     private LocalDate dueDate;
-    private LocalDate returnDate;
+
+    private LocalDate returnDate; // nullable
 
     public IssueRecord() {}
-    // Getters and setters ...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
 }
